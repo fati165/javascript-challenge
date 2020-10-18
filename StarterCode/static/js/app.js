7ca6059bec106b41c2be0a2780beac3050d2a019
fati165 <gdonato38@gmail.com>
2020-10-17 23:18:05 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,14 @@
 //Using the UFO dataset provided in the form of an array of JavaScript objects,
 // write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
+//Make sure you have a column for date/time, 
+//city, state, country, shape, and comment at 
+//the very least.
 
 // from data.js
 var tableData = data;
 
+console.log(data)
 // YOUR CODE HERE!
 // select tbody 
 tbody = d3.select("tbody")
@@ -22,3 +26,20 @@ function ufotable(input){
     })
 
 }
+//Make buttons happen
+var button = d3.select('#filter-btn');
+var input1 = d3.select('#datetime')
+
+//make another function for the buttons to show stuff
+button.on('click', filts);
+function filts(){
+    //prevent page from refreshing
+    d3.event.preventDefault();
+    tbody.html("");
+    //filter date and time for search!
+    var filteredData = data.filter(data => data.datetime === input1);
+
+    console.log(input1);
+}
+
+
